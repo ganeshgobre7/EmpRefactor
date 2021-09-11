d8a840ef051a2376c86bb394625301e594541b34
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-11 13:34:22 +0530

    Full time daily wage calculated

--- EmplRefactor/Calculatesalary.cs
+++ EmplRefactor/Calculatesalary.cs
@@ -7,6 +7,9 @@ namespace EmplRefactor
     class Calculatesalary
     {
         int empPresent = 1;
+        int full_Time = 8;
+        int per_Hour_Sal = 20;
+        int one_Day_Sal = 0;
         public void check()
         {
             Random random = new Random();
@@ -14,7 +17,8 @@ namespace EmplRefactor
             
             if (empCheck==empPresent)
             {
-                Console.WriteLine("Employee is Present");
+                one_Day_Sal = full_Time * per_Hour_Sal;
+                Console.WriteLine("Full time employee One Day Salary is :"+ one_Day_Sal); ;
             }
             else
             {
